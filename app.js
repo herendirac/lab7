4a9804b8ab7fc1130f4ce206bafc9c5e7748c429
EC2 Default User <ec2-user@ip-172-31-21-138.us-east-2.compute.internal>
2019-11-03 05:19:42 +0000

    second

--- app.js
+++ app.js
@@ -4,15 +4,20 @@ app.set("view engine", "ejs");
 app.use(express.static("public")); //folder for images, css, js
 
 const request = require('request');
+const shufflefy = require('shufflefy');
 
 //routes
 app.get("/", async function(req, res){
     
- let parsedData = await getImages("otters");
+ let keyword = getWord();
+    let parsedData = await getImages(keyword, "all");
+    let shuffled = shufflefy(parsedData.hits);
  
- console.dir("parsedData: " + parsedData); //displays content of the object
-    
- res.render("index", {"image":parsedData.hits[0].largeImageURL});
+ //console.dir("parsedData: " + parsedData); //displays content of the object
+ 
+    // shuffle array
+ res.render("index", {
+        "keyword":keyword,"image1":shuffled[0],"image2":shuffled[1],"image3":shuffled[2],"image4":shuffled[3]});
             
 }); //root route
 
@@ -21,20 +26,21 @@ app.get("/results", async function(req, res){
     
     //console.dir(req);
     let keyword = req.query.keyword; //gets the value that the user typed in the form using the GET method
-    
+    let shuffled = shufflefy(parsedData.hits);
     let parsedData = await getImages(keyword);
-
-    res.render("results", {"images":parsedData});
+    let orientation = req.query.orientation.toLowerCase();
+    //array shuffle
+    res.render("results", {"keyword":keyword,"orientation": orientation,"image1":shuffled[0],"image2":shuffled[1],"image3":shuffled[2],"image4":shuffled[3]});
     
 });//results route
 
 
 //Returns all data from the Pixabay API as JSON format
-function getImages(keyword){
+function getImages(keyword, orientation){
     
     
     return new Promise( function(resolve, reject){
-        request('https://pixabay.com/api/?key=5589438-47a0bca778bf23fc2e8c5bf3e&q='+keyword,
+        request('https://pixabay.com/api/?key=5589438-47a0bca778bf23fc2e8c5bf3e&q='+keyword+'&orientation='+orientation,
                  function (error, response, body) {
     
             if (!error && response.statusCode == 200  ) { //no issues in the request
@@ -49,8 +55,8 @@ function getImages(keyword){
                 
             } else {
                 reject(error);
-                console.log(response.statusCode);
-                console.log(error);
+                console.warn(response.statusCode);
+                console.error(error);
             }
     
           });//request
@@ -59,6 +65,13 @@ function getImages(keyword){
     
 }
 
+function getWord(){
+    let arr = ["Painterly", "abstract", "Botanical", "Realism"];
+    
+    let shuffledArr =shufflefy(arr);
+    
+    return shuffledArr[0];
+}
 
 //starting server
 app.listen(process.env.PORT, process.env.IP, function(){
